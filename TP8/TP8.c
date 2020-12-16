fbbc09d5c9bce1a69f9808a3018107ee6d88fa80
CChevr <cedricus@msn.com>
2020-12-16 20:11:26 +0100

    dlopen

--- TP8/TP8.c
+++ TP8/TP8.c
@@ -1,6 +1,7 @@
 #define _DEFAULT_SOURCE
 #include <dirent.h>
 #include <dlfcn.h>
+#include <assert.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +12,7 @@
 typedef struct{
     char * name;
     char sym;
-    int nb_args;
+    int nbArgs;
 }Function;
 
 static int sel_lib(const struct dirent *file){
@@ -33,9 +34,63 @@ static int sel_lib(const struct dirent *file){
     return 0;
 }
 
+int allocTabFunc(Function ** funcTab, int nbFunc){
+    /*
+    Alloue l'espace mémoire nécessaire à un tableau de Function de nbFunc éléments.
+    Retourne 1 si l'allocation s'est bien déroulée, 0 sinon.
+    */
+    *funcTab = (Function*) malloc(nbFunc * sizeof(funcTab));
+
+    if(NULL == *funcTab){
+        printf("Erreur lors de l'allocation du tableau de Function.\n");
+        return 0;
+    }
+}
+
+int initTabFunc(Function * funcTab, struct dirent ** namelist, int nbElem){
+    /*
+    Initialise le tableau de Function funcTab à l'aide de namelist
+    */
+    void * lib;
+    int i;
+
+    assert(NULL != funcTab);
+    assert(NULL != namelist);
+    assert(nbElem >= 0);
+    
+    for(i = 0; i < nbElem; i++){
+        lib = dlopen(namelist[i]->d_name, RTLD_LAZY);
+
+        if(!lib){
+            fprintf(stderr, "Erreur lors du chargement de la lib : %s.\n", namelist[i]);
+            return 0;
+        }
+        
+    }
+    return 1;
+}
+
+int initFunc(Function * func, char * name, char sym, int nbArgs){
+    /*
+    Initialise la func passé en parametre.
+    Retourne 1 si l'initialisation s'est bien passé, 0 sinon.
+    */
+    if(NULL == func)
+        return 0;
+    
+    strcpy(func->name, name);
+    func->sym = sym;
+    func->nbArgs = nbArgs;
+
+    return 1;
+}
+
+
+
 int main(int argc, char * argv[]){
-    Function * func_tab;
+    Function * funcTab;
     struct dirent ** namelist;
+    void * lib;
     int n;
     int i;
 
@@ -45,9 +100,9 @@ int main(int argc, char * argv[]){
         return 0;
     }
 
-    for(i = 0; i < n; i++){
-        printf("%s\n", namelist[i]->d_name);
-    }
+
+    /*Libération funcTab*/
+    free(funcTab);
 
     /*Libération de namelist*/
     while(n--){
