665e347cf094ff72dfaf06a71fec328fedbdd55f
CChevr <cedricus@msn.com>
2020-12-16 17:20:25 +0100

    tests

--- TP8/TP8.c
+++ TP8/TP8.c
@@ -1,7 +1,27 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char * argv[]){
+    struct dirent ** namelist;
+    int n;
+    int i;
 
+    n = scandir(".so", &namelist, NULL, alphasort);
+    if(-1 == n){
+        fprintf(stderr, "Aucun fichier trouvé.\n");
+        return 0;
+    }
+
+    for(i = 0; i < n; i++){
+        printf("%s\n", namelist[i]->d_name);
+    }
+
+    /*Libération de namelist*/
+    while(n--){
+        free(namelist[n]);
+    }
+
+    free(namelist);
     return 1;
 }
