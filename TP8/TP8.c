0ac042438966eb5c9077e106ba7292ea2e115bab
CChevr <cedricus@msn.com>
2020-12-16 18:46:39 +0100

    liste fichiers "*.so" dans le rpt ./plugins

--- TP8/TP8.c
+++ TP8/TP8.c
@@ -1,14 +1,38 @@
+#define _DEFAULT_SOURCE
 #include <dirent.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PATH    "./plugins/"
+#define EXT     ".so"
+
+
+static int sel_lib(const struct dirent *file){
+    /*
+    prends un direct en parametre, retourne 1 si l'extension du fichier
+    correspondant est la même que EXT (.so), 0 sinon.
+    */
+    if(!file)
+        return 0;
+    
+    char * ext = strchr(file->d_name, '.');
+
+    if(!ext)
+        return 0;
+    
+    if(strcmp(ext, EXT) == 0)
+        return 1;
+
+    return 0;
+}
 
 int main(int argc, char * argv[]){
     struct dirent ** namelist;
     int n;
     int i;
 
-    n = scandir(".so", &namelist, NULL, alphasort);
+    n = scandir(PATH, &namelist, sel_lib, alphasort);
     if(-1 == n){
         fprintf(stderr, "Aucun fichier trouvé.\n");
         return 0;
---
+++ TP8/plugins/affiche.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int eval(int* args){
+    printf("le chiffre est : %d\n", *args);
+}
+
+
+
+int main(int argc, char * argv[]){
+
+    return 1;
+}
